975b05e3ee8bcf8d93efa95af66479c1e3a3ae32
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2023-12-31 17:57:26 +0530

    Character to ASCII value conversion

---
+++ Day_1/Character to ASCII conversion.cpp
@@ -0,0 +1,22 @@
+// Write a program that takes character as input and print the ASCII value of that character.
+
+#include<bits/stdc++.h>
+
+int main(){
+    char ch;
+    std::cin>>ch;
+
+    std::cout<<int(ch)<<std::endl;
+    return 0;
+}
+
+/**
+ * Input: p
+ * Output: 112
+ * -----------------------
+ * Input: y
+ * Output: 121
+ * -----------------------
+ * Input: A
+ * Output: 65
+*/
